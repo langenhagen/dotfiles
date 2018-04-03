6bd29fb81a7a67c4275b24922aef6bd11f0fc672
Andreas Langenhagen <andreas.langenhagen@here.com>
2018-04-03 10:25:46 +0200

    Add cmd plugin to sublimetext
    The self-written cmd plugin starts an Applescript app, that,
    given a certain directory, opens a new iTerm2 tab at the given
    directory.

---
+++ sublimetext3-config/Packages/User/Cmd.py
@@ -0,0 +1,16 @@
+import os
+import sublime
+import sublime_plugin
+import subprocess
+
+class CmdCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        ''' gets the directory path of the current text file and
+            calls on an applescript app that opens the given directory in a new iTerm2 tab.
+        '''
+        file_name = self.view.file_name()
+        path = file_name.split(os.sep)
+        path.pop()
+        current_directory = os.sep.join(path)
+        subprocess.call('open -a /Users/langenha/code/scripts/OpenIterm2.app ' + current_directory, shell=True)
+        

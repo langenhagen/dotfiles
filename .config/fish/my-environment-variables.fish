6554579a3b4a1f0a9e00d13980878a6444dc50d4
Andreas Langenhagen <andreas.langenhagen@here.com>
2018-09-26 16:56:17 +0200

    Remove superfluous environment variables
    Also, put icecc search paths at the beginning of a the PATH variable,
    not at its end.

--- .config/fish/my-environment-variables.fish
+++ .config/fish/my-environment-variables.fish
@@ -1,6 +1,6 @@
 # My personal fish environment variables file for my Mac-workmachine
 # author: andreasl
-# version: 18-09-11
+# version: 18-09-26
 
 # source this file into the config.fish
 
@@ -14,6 +14,7 @@ setenv PROTOFILES_DIR_PATH "$HOME/personal/Dev/_Protofiles"
 setenv PLAYGROUND_DIR_PATH "$HOME/personal/Dev/_playgrounds"
 
 setenv JAVA_HOME '/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home'
+
 # The following variable Z_SCRIPT_PATH must actually be set before sourcing .config/fish/conf.d/omf.fish
 # I set it directly inside the omf.fish file at the top. That seems to work :)
 # I still let it here for doc purposes for now.  Feel free to remove :)
@@ -26,14 +27,8 @@ setenv NDK_ROOT $ANDROID_NDK_HOME
 set -gx PATH $PATH $ANDROID_HOME/tools
 set -gx PATH $PATH $ANDROID_HOME/platform-tools
 set -gx PATH $PATH '/Users/langenha/personal/Dev/scripts'
-#setenv ANDROID_SERIAL 024475e094d2743e      # that's the LG Nexus with PTM #245
-setenv ANDROID_SERIAL H6AKCV0015736RJ      # that's the ASUS Tango
 
 setenv OPENSSL_ROOT_DIR '/usr/local/Cellar/openssl/1.0.2h_1/'
-setenv WORKSPACE "$HOME/code/sparta" # used for sparta CMake scripts to identify workspace
 
-# ccache memcached settings - for ivi-mobile-frameworks/apigen team shared build cache
-set -gx CCACHE_MEMCACHED_CONF '--SERVER=apigen-memcached.ad.here.com'
-set -gx CCACHE_BASEDIR '/Users/langenha/code/sparta'
 #setenv CCACHE_PREFIX icecc
-#set -gx PATH $PATH /usr/local/opt/icecream/libexec/icecc/bin
+#set -gx PATH /usr/local/opt/icecream/libexec/icecc/bin $PATH

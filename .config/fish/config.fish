de77adf40750ce22cfc4fad7effdd81ba3b3bdb6
Andreas Langenhagen <andreas@langenhagen.cc>
2018-10-20 19:05:05 +0200

    Source miniconda fish integration, if available
    Also, remove superfluous comment in sourcing iterm2 fish entegration.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # My personal fish config file.
 # Should work on all platforms, including Mac OS with iterm2.
 # author: andreasl
-# version: 18-09-27
+# version: 18-10-19
 
 set fish_greeting "" # deactivates the fish welcome message
 
@@ -13,7 +13,8 @@ source ~/.config/fish/my-abbreviations.fish
 
 source ~/.config/fish/vimlike-bookmarks.fish
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish # iterm2 shell integration
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/miniconda3/etc/fish/conf.d/conda.fish ; and source {$HOME}/miniconda3/etc/fish/conf.d/conda.fish
 
 ######### the initial commands #####################################################################
 

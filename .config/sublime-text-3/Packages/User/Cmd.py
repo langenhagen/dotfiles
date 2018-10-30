2843d589cf489ab107f46ff9c317a362991e2011
Andreas Langenhagen <andreas@langenhagen.cc>
2018-10-30 19:24:46 +0100

    Open Terminal Sublime on Linux
    Enable Sublime Text to open KDE's Konsole with the path of the current
    document set up from the tab bar. Disable Mac's Open iTerm shell command
    until I know how to distinguish between operating systems in Python.

--- .config/sublime-text-3/Packages/User/Cmd.py
+++ .config/sublime-text-3/Packages/User/Cmd.py
@@ -5,22 +5,18 @@ import subprocess
 
 class CmdCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        ''' gets the directory path of the current text file and
-            calls on an applescript app that opens the given directory in a new iTerm2 tab.
+        ''' Gets the directory path of the current text file and, depending on the platform,
+            either, if on Linux, opens the KDE's Konsole
+            or, if on Mac, calls an applescript that opens the given directory in a new iTerm2 tab.
         '''
         file_name = self.view.file_name()
         path = file_name.split(os.sep)
         path.pop()
         current_directory = os.sep.join(path)
 
-        # TODO make it work on Linux as well:
-        # e.g. check platform.
-        #   if platform is OSX go normal path,
-        #   else if platform is Linux open preferrred terminal in some way
-        #   else do nothing
-
-        # TODO somehow make the path ot OpenIterm2.app not hard coded
-        # options:
-        #  use environment variable
-        #  put the OpenIterm2.app into a relative dir
-        subprocess.call('open -a "$SCRIPTS_DIR_PATH/OpenIterm2.app" ' + current_directory, shell=True)
+        # TODO if Linux
+        subprocess.call('konsole --workdir ' + current_directory, shell=True)
+        #elif MAC
+        # subprocess.call('open -a "$SCRIPTS_DIR_PATH/OpenIterm2.app" ' + current_directory, shell=True)
+        # else
+        #     pass

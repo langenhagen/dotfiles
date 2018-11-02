24b2f400d2a194155dd5845d8380a558841e5b29
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2018-11-02 18:39:20 +0100

    Add new cpp env vars for c1
    Also separate celeraone varibles into another case statement. if
    statements in fish do not seem to handle string contaqins elements very
    well.

--- .config/fish/my-environment-variables.fish
+++ .config/fish/my-environment-variables.fish
@@ -7,7 +7,7 @@
 set -gx PATH $PATH /usr/local/sbin
 
 switch (uname -n)
-    case "*barn-ultra*" "*celeraone*"
+    case "*barn-ultra*"
 
         setenv TRICKS_FILE_PATH "$HOME/Dev/Zeugs/tricks.txt"
         setenv ONE_LINE_HELP_FILE_PATH "$HOME/Dev/Zeugs/one-line-help.txt"
@@ -17,6 +17,37 @@ switch (uname -n)
         setenv PLAYGROUND_DIR_PATH "$HOME/Dev/_playgrounds"
 
         setenv SCRIPTS_DIR_PATH "$HOME/Dev/scripts"
+
+    case  "*celeraone*"
+
+        setenv TRICKS_FILE_PATH "$HOME/Dev/Zeugs/tricks.txt"
+        setenv ONE_LINE_HELP_FILE_PATH "$HOME/Dev/Zeugs/one-line-help.txt"
+        setenv BUCKET_LIST_FILE_PATH "$HOME/Barn/Notes/bucket-list.txt"
+        setenv JOURNAL_FILE_PATH "$HOME/Barn/Notes/Journal.txt"
+        setenv PROTOFILES_DIR_PATH "$HOME/Dev/_Protofiles"
+        setenv PLAYGROUND_DIR_PATH "$HOME/Dev/_playgrounds"
+
+        setenv SCRIPTS_DIR_PATH "$HOME/Dev/scripts"
+
+        # Distcc Configuration
+        # ====================
+
+        # Enable local cache to use Distcc.
+        setenv CCACHE_PREFIX distcc
+
+        # Remote servers used for compilation.
+        setenv DISTCC_HOSTS "192.168.30.70/16,lzo --localslots/4"
+
+        # Enable debug mode.
+        # Useful when Distcc cannot connect to remote servers.
+        # setenv DISTCC_VERBOSE 1
+
+
+        # Integration Tests
+        # =================
+
+        # Path where engine binaries are stored.
+        setenv C1_CRED_PREFIX /opt/c1/engine
 end
 
 setenv LANG "en_US.UTF-8"

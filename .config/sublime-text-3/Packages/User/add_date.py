cd040e943e37c1baf9a3c657ba7e1207414656d5
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2018-11-02 10:37:00 +0100

    Change sublime text user defined function add_date
    Move the day of the week to the beginning,
    e.g. Friday, 02 November 2018.

--- .config/sublime-text-3/Packages/User/add_date.py
+++ .config/sublime-text-3/Packages/User/add_date.py
@@ -2,7 +2,7 @@ import datetime, getpass
 import sublime, sublime_plugin
 class AddDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.date.today().strftime("%d %B %Y (%A)") } )
+        self.view.run_command("insert_snippet", { "contents": "%s" %  datetime.date.today().strftime("%A, %d %B %Y") } )
 
 class AddTimeCommand(sublime_plugin.TextCommand):
     def run(self, edit):

34f5ecd8323527938f475401c63ff34862d2a583
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2018-11-09 13:50:45 +0100

    Add abbreviations for updating c1 repos

--- .config/fish/my-abbreviations.fish
+++ .config/fish/my-abbreviations.fish
@@ -126,6 +126,9 @@ abbr -a gri 'git rebase --interactive HEAD~10'
 abbr -a grc 'git rebase --continue'
 abbr -a gra 'git rebase --abort'
 
+abbr -a gpra 'find . -type d -iname "*.git" -execdir bash -c \'printf "\033[1m${PWD}\033[0m\n"; git pull --rebase;\' \;'
+abbr -a sco 'cd ~/c1; find . -type d -iname "*.git" -execdir bash -c \'printf "\033[1m${PWD}\033[0m\n"; git pull --rebase;\' \;'
+
 abbr -a grsh 'git reset --soft HEAD~1'
 abbr -a grhh 'git reset --hard HEAD'
 abbr -a grhh1 'git reset --hard HEAD~1'

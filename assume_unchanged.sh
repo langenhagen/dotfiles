6df87eb5f40cfbecfcbda029c70b505186127d20
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2018-11-15 09:39:32 +0100

    Add assume_unchanged.sh
    Add a script that bundles eventually repeated calls to
    `git update-index` --assume-unchanged`.
    This is helpful when further changes to some files should be ignored
    from the version but the files should not entirely ignored.

---
+++ assume_unchanged.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Sets git to assume files in an array to be unchanged, or
+# if the flag `-no` is passed, sets git not to assume that files in an array are unchanged.
+#
+# Chhange the array to your likings.
+#
+# author: andreasl
+# version: 18-11-15
+
+files=(
+    ".config/sublime-text-3/Packages/User/Preferences.sublime-settings"
+    ".config/konsolerc"
+    )
+
+
+if [ "$1" == '-no' ]; then
+    for file in ${files[@]} ; do
+        git update-index --no-assume-unchanged ${file}
+    done
+else
+    for file in ${files[@]} ; do
+        git update-index --assume-unchanged ${file}  # ignore further changes to a file
+    done
+fi
+
+
+

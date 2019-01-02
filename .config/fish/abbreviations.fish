58698c9e995e916427cb747eb504b208a9122692
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2019-01-02 15:43:01 +0100

    Simplified fish abbreviation editjrn

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -79,7 +79,7 @@ abbr -a mnm 'minimerk \''
 abbr -a bkt 'bucket \''
 abbr -a b 'bucket \''
 abbr -a tks 'tricks'
-abbr -a editjrn "vim -R \"+normal G\\\$\" -p $JOURNAL_FILE_PATH"
+abbr -a editjrn "vim -R '+normal G\$' -p $JOURNAL_FILE_PATH"
 abbr -a olh 'one-line-help'
 abbr -a alh 'add-to-one-line-help \''
 

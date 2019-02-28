d566da7c2281c95c6314a570fecc24a90d713090
Andreas Langenhagen <andreas@langenhagen.cc>
2019-02-28 17:38:59 +0100

    Remove several fish abbreviations
    Apparently, having several fish abbreviations can slow down the fish
    loading time. Thus, removing some of them might improve loading time for
    fish. This might especially turn out good on ssh connections to slow
    machines.

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -5,8 +5,6 @@
 #
 # author: andreasl
 
-alias fuckoff='for i in (seq 6); tput ll; openssl rand 32;  sleep 0.1; end; printf \'\\nBye :)\\n\'; shutdown 0'
-
 if [ (uname) = 'Darwin' ]
     abbr -a xcode 'open -a Xcode'
     abbr -a xc 'open -a Xcode'
@@ -55,11 +53,9 @@ abbr -a tmls 'tmux ls'
 abbr -a tmk 'tmux kill-session -t'
 abbr -a tmw 'tmux kill-window -t'
 
-abbr -a find 'find -L . -iname'
-abbr -a fnd 'find -L . -iname'
 abbr -a f 'find -L . -iname'
 abbr -a fr 'find -L -E . -iregex'
-abbr -a fnd1 'find -L . -maxdepth 1 -iname'
+abbr -a f1 'find -L . -maxdepth 1 -iname'
 abbr -a tree 'tree -C -a'
 abbr -a tre 'tree -C -a'
 abbr -a t3 'tree -C -a -L 3'
@@ -69,10 +65,8 @@ abbr -a t 'tig'
 abbr -a ts 'tig show'
 abbr -a r 'ranger'
 abbr -a e 'vim -R "+normal :Explore\$" .'
-abbr -a p 'printf (command -v python)'  version: '; and python --version; and python'
 abbr -a gitup 'pwd ; cd (git rev-parse --show-toplevel)'
 abbr -a gitp  'pwd ; cd (git rev-parse --show-toplevel)'
-abbr -a ccat 'pygmentize -O style=native -f console256 -g'
 abbr -a cct 'pygmentize -O style=native -f console256 -g'
 abbr -a bp 'bpython'
 abbr -a json 'json_pp'  # pretty-print json output
@@ -95,7 +89,6 @@ abbr -a cd. 'cd ..'
 # grep -Hirns - like abbreviations
 abbr -a g 'grep'
 abbr -a h 'grep -HiRns --exclude-dir=".git" --color'
-abbr -a hI 'grep -HiRnsI --exclude-dir=".git" --color'
 abbr -a ch 'grep -HiRns --exclude-dir=".git" --include={\*.h,\*.hpp,\*.c,\*.cpp,\*.cc,\*.m,\*.mm,\*.pch,\*.java,\*.swift,\*.kt} --color'
 abbr -a cmh 'grep -HiRns --exclude-dir=".git" --include={CMakeCache.txt,CMakeLists.txt,\*.cmake} --color'
 abbr -a ph 'grep -HiRns --exclude-dir=".git" --include=\*.py --color'
@@ -113,13 +106,10 @@ abbr -a gss 'git stash'
 abbr -a gsa 'git stash apply'
 
 abbr -a gco 'git checkout'
-abbr -a gcs 'git checkout staging'
-abbr -a gcos 'git checkout staging'
 abbr -a gcm 'git checkout master'
 abbr -a gcom 'git checkout master'
 abbr -a gm 'git checkout master'
 
-abbr -a gcb 'git checkout -b'
 abbr -a gbn 'git checkout -b'
 abbr -a gnb 'git checkout -b'
 
@@ -129,13 +119,11 @@ abbr -a gbt 'git checkout -b tmp; or git checkout tmp; git branch'  # git branch
 abbr -a gct 'git checkout -b tmp; or git checkout tmp; git branch'  # git checkout tmp
 abbr -a gdt 'git checkout master; git branch -D tmp; git branch'   # git delete tmp
 
-abbr -a gps 'git push origin HEAD:refs/for/staging'
 abbr -a gpm 'git push origin HEAD:master'
 abbr -a gprm 'git push origin HEAD:refs/for/master'
 
 abbr -a gp 'git pull --rebase'
 abbr -a gpl 'git pull --rebase'
-abbr -a gpls 'git pull --rebase origin staging'
 abbr -a gplm 'git pull --rebase origin master'
 
 abbr -a gri 'git rebase --interactive HEAD~10'
@@ -165,9 +153,6 @@ abbr -a rpc 'repo status ; repo diff ; repo forall -c "printf \"\033[1;36m`git r
 
 abbr -a gr 'git review -d'
 abbr -a sfr "git review master --reviewers (git log -n40 --pretty=format:'%ae' | sort | uniq -c | sort -nr | awk '{print \$2}' | head -8)"
-
-abbr -a foldmsg 'grep -v "^#" (git rev-parse --show-toplevel)/.git/COMMIT_EDITMSG | fold -s -w 72   | sed \'s/[ \t]*$//\' > (git rev-parse --show-toplevel)/.git/COMMIT_EDITMSG;'
-abbr -a rmlines 'sed -i \'/ \/rm/d\' (git diff --name-only HEAD)  # remove lines that contain the string \'/rm\''
 
 abbr -a proto 'cd "$PROTOFILES_DIR_PATH" ; find "$PROTOFILES_DIR_PATH" -name "*_proto.*"'
 abbr -a prt 'cd "$PROTOFILES_DIR_PATH" ; find "$PROTOFILES_DIR_PATH" -name "*_proto.*"'

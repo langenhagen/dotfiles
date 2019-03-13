36d0753bdea34fd75d7a90b104c5bd7133224bac
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2019-03-13 11:26:47 +0100

    Add fish abbr qex  for cd-ing into c1-exceptions

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -172,6 +172,7 @@ case  "*celeraone*"
     abbr -a qap 'cd-into-c1-project authproxy'
     abbr -a qc 'cd-into-c1-project cre-api'
     abbr -a qe 'cd-into-c1-project entitlement'
+    abbr -a qex 'cd-into-c1-project exceptions'
     abbr -a qf 'cd-into-c1-project frontend'
     abbr -a ql 'cd-into-c1-project lua'
     abbr -a qm 'cd-into-c1-project metrics'

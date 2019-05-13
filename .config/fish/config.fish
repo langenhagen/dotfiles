675aca1aa0e4dc0aa8a07d78bf65a7261619e7dc
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2019-05-13 10:59:16 +0200

    Remove fish config bootstrapping section
    Remove bootstrapping from config.fish again, since it led to hanging up
    and breaking the Linux session on my work machine. I consider a
    dedicated installation safer.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,21 +19,4 @@ source ~/.config/fish/vimlike-bookmarks.fish
 test -e $HOME/.iterm2_shell_integration.fish ; and source $HOME/.iterm2_shell_integration.fish
 test -e $HOME/miniconda3/etc/fish/conf.d/conda.fish ; and source $HOME/miniconda3/etc/fish/conf.d/conda.fish
 
-### bootstrapping ##################################################################################
-
-# fisher, the fish package manager
-# found here: https://github.com/jorgebucaran/fisher
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-# z
-# found here: https://github.com/jethrokuan/z
-if not functions -q z
-    fisher add jethrokuan/z
-end
-
 ### the initial commands ###########################################################################
-

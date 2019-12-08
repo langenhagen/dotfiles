6a2ffa8e6d30eab57ea78138a9e73fe2e82b6f6f
Andreas Langenhagen <andreas@langenhagen.cc>
2019-12-08 02:40:00 +0100

    Remove open-TODOs-file.desktop

--- link-files.sh
+++ link-files.sh
@@ -32,7 +32,6 @@ elif [[ "$(uname -n)" =~ 'celeraone' ]]; then
     rm "$HOME/.config/reposets"
     ln -sf "$dotfiles_path/.config/reposets-c1" "$HOME/.config/reposets";
 
-    ln -sf "$dotfiles_path/.config/autostart/open-TODOs-file.desktop" "$HOME/.config/autostart/open-TODOs-file.desktop";
     ln -sf "$dotfiles_path/.config/autostart/open-day-notes-file.desktop" "$HOME/.config/autostart/open-day-notes-file.desktop";
     ln -sf "$dotfiles_path/.config/autostart/slack.desktop" "$HOME/.config/autostart/slack.desktop";
 fi

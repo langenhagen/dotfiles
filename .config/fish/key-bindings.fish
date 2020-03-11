1882a2e22709a7eeb6c7d774072b73336f5775ba
Andreas Langenhagen <andreas@langenhagen.cc>
2020-03-11 22:07:12 +0100

    Make fish alt+arrow keys move a BIGWORD

--- .config/fish/key-bindings.fish
+++ .config/fish/key-bindings.fish
@@ -10,4 +10,7 @@ function fish_user_key_bindings
     # as a safety feature, press ctrl+d quickly 2 times exit fish
     bind \cd delete-char
     bind \cd\cd exit
+
+    bind \e\[1\;3D backward-bigword
+    bind \e\[1\;3C forward-bigword
 end

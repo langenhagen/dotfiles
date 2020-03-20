b4d22f9b4b00c28705753785c9660bba203bd2ee
Andreas Langenhagen <andreas@langenhagen.cc>
2020-03-20 14:12:17 +0100

    Remove some functions from functions.fish
    I ported them to bash functions into my scripts repository.

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -22,17 +22,6 @@ function read_confirm
   end
 end
 
-function tricks
-    # Show the tricks file and enable grepping on it.
-
-    if test (count $argv) -eq 0
-        # -R readonly
-        vim -R "+call feedkeys('/')" "$TRICKS_FILE_PATH"
-    else if test (count $argv) -gt 0
-        grep -in --color "$argv" $TRICKS_FILE_PATH
-    end
-end
-
 function journal
     # Show the journal file or append given input to it.
     # There must be a trailing newline at the end of the file or else output might be fucked up.
@@ -67,29 +56,6 @@ function bucket
     end
 end
 
-function one-line-help
-    # Grep for the given arguments on the one-line-help.txt file.
-    if test (count $argv) -eq 0
-        vim -R "$ONE_LINE_HELP_FILE_PATH"  # -R readonly
-    else
-        set results (grep -i --color=never "$argv[1]" "$ONE_LINE_HELP_FILE_PATH")
-        for arg in $argv
-            set results (printf -- '%s\n' $results | grep -i --color=never "$arg")
-        end
-        printf -- '%s\n' $results
-    end
-end
-
-function add-to-one-line-help
-    # Add a line to the one-line-help.txt file.
-    echo $argv >> "$ONE_LINE_HELP_FILE_PATH"
-
-    set tmp_olh_bak_path (mktemp /tmp/one-line-help-backup.XXXXXXXX)
-    cat $ONE_LINE_HELP_FILE_PATH >> $tmp_olh_bak_path
-    sort -u -o $ONE_LINE_HELP_FILE_PATH $tmp_olh_bak_path
-    rm $tmp_olh_bak_path
-end
-
 function addabr
     # Add a line to the abbreviations file and activate the abbreviation.
     echo "abbr -a $argv" >> ~/.config/fish/abbreviations.fish
@@ -105,13 +71,6 @@ end
 function rem
     # print a string in bold on the console in order to remember things.
     printf "\n\e[1m$argv\e[0m\n"\n
-end
-
-function printn
-    # Print a given string a given number of times.
-    for i in (seq $argv[2])
-        printf $argv[1]
-    end
 end
 
 function fd

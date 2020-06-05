84ff738af7b1da5b995d3fa823bf361ae368956b
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-06-05 15:01:35 +0200

    Change the fish prompt

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -4,6 +4,36 @@
 #
 # author: andreasl
 
+function __fish_prompt_my --description "Write out my, andreasl's,  prompt"
+    set -l last_pipestatus $pipestatus
+    set -l normal (set_color normal)
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '>'
+    if contains -- $USER root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # Write pipestatus
+    set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+    printf '%s%s%s%s%s%s%s' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
+end
+
+function fish_prompt
+   set -l last_status $status
+   if set -q CONDA_LEFT_PROMPT
+           __conda_add_prompt
+   end
+   return_last_status $last_status
+   __fish_prompt_my
+end
+
+
 function read_confirm_prompt
     echo 'Do you want to continue? [yY/nN] '
 end

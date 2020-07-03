38519c56b0c1a2ea86db81a8da9e721a5e03cdea
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-07-04 00:17:19 +0200

    Refactor previewer.sh of `lf`

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# As simple previewer I use in the cli file explorer `lf`.
+#
+# author: andreasl
 
 unset COLORTERM
-# vim --cmd 'set t_ti= t_te=' +redraw +q "$1"
 
 command -v batcat && batcat --color=always --style='numbers,changes,header' "$1" && exit 0
 cat "$1"

f1be0729932df8164249573f76eeaee009c7e0da
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-07-04 22:14:10 +0200

    Disable coloring in lf previewer
    Currently, coloring behaves incorrectly. It omits characters and
    highlights very sparse. Disable coloring to at least print the right
    characters.

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # As simple previewer I use in the cli file explorer `lf`.
 #
+# TODO: make coloring work correctly in lf.
+#
 # author: andreasl
 
 unset COLORTERM
 
-command -v batcat && batcat --color=always --style='numbers,changes,header' "$1" && exit 0
+command -v batcat && batcat --color=auto --style='numbers,changes,header' "$1" && exit 0
 cat "$1"

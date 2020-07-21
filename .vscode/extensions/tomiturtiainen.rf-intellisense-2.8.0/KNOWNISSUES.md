9e2ec5e807a971e01ebcb2e7885dd8ea1f858a3f
Andreas Langenhagen <andreas@langenhagen.cc>
2020-07-21 15:50:46 +0200

    Remove .vscode/extensions from version control
    Rather use the vscode-extensions.txt and the accompanying scripts.

--- .vscode/extensions/tomiturtiainen.rf-intellisense-2.8.0/KNOWNISSUES.md
+++
@@ -1,6 +0,0 @@
-# Known Issues
-
-* Test case templates are not parsed correctly
-* For loops are not parsed correctly
-* Can't find keywords defined in java libraries
-* Gherkin style is not yet supported
--- .vscode/extensions/tomiturtiainen.rf-intellisense-2.8.0/out/src/migration-helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const python_keywords_1 = require("./migrations/python-keywords");
-function runMigrations() {
-    python_keywords_1.default();
-}
-exports.runMigrations = runMigrations;
-//# sourceMappingURL=migration-helper.js.map

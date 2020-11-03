2f5c0c904e71afad2d93d9657a7fcf470752ebfb
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-03 11:20:28 +0100

    Simplify autokey script Insert-Random-Word.py

--- .config/autokey/data/My-Global-Scripts/Insert-Random-Word.py
+++ .config/autokey/data/My-Global-Scripts/Insert-Random-Word.py
@@ -1,4 +1,2 @@
-system.exec_command("/bin/bash -c '${HOME}/.pyenv/shims/python ${HOME}/Dev/scripts/random_word.py 4 | tee /home/andreasl/.random_word'")
-
-output = system.exec_command("head -1 /home/andreasl/.random_word")
+output = system.exec_command("/bin/bash -c '${HOME}/.pyenv/shims/python ${HOME}/Dev/scripts/random_word.py 4'")
 keyboard.send_keys(output)

4e257c0b1aff8dc34fb12f0298300eda52a66e29
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-04-27 15:55:11 +0200

    Improve a check in `config.fish`
    The old check still wrote to terminal despite `1>/dev/null 2>&1`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,6 +19,6 @@ test -e ~/.rbenv/bin/rbenv; and status --is-interactive; and source (~/.rbenv/bi
 
 command -v pyenv 1>/dev/null 2>&1; and pyenv init - | source
 command -v direnv 1>/dev/null 2>&1; and eval (direnv hook fish)
-nvm --version 1>/dev/null 2>&1; and nvm use lts 1>/dev/null
+command -v nvm 1>/dev/null 2>&1; and nvm use lts 1>/dev/null
 
 ### the initial commands ###########################################################################

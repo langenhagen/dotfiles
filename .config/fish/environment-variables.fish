e53715be17563707a3020073c532ad4e14f25a5f
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-08-28 11:19:36 +0200

    Use `set -x` as opposed to `setenv` in fish configs
    `setenv` is apparently just a compatibility layer for `csh` users. It's
    also a bit slower because it's just a fish function as opposed to a
    builtin which `set` is.

--- .config/fish/environment-variables.fish
+++ .config/fish/environment-variables.fish
@@ -6,16 +6,16 @@
 
 switch (uname -n)
     case "*barn-ultra*" "*work*"
-        setenv BUCKET_LIST_FILE_PATH "$HOME/Barn/Notes/bucket-list.txt"
-        setenv CHECK_DIR_PATH "$HOME/Dev/check"
-        setenv ONE_LINE_HELP_FILE_PATH "$HOME/Dev/Zeugs/one-line-help.txt"
-        setenv PROTOFILES_DIR_PATH "$HOME/Dev/protofiles"
-        setenv SCRIPTS_DIR_PATH "$HOME/Dev/scripts"
-        setenv TRICKS_FILE_PATH "$HOME/Dev/Zeugs/tricks.sh"
+        set -x BUCKET_LIST_FILE_PATH "$HOME/Barn/Notes/bucket-list.txt"
+        set -x CHECK_DIR_PATH "$HOME/Dev/check"
+        set -x ONE_LINE_HELP_FILE_PATH "$HOME/Dev/Zeugs/one-line-help.txt"
+        set -x PROTOFILES_DIR_PATH "$HOME/Dev/protofiles"
+        set -x SCRIPTS_DIR_PATH "$HOME/Dev/scripts"
+        set -x TRICKS_FILE_PATH "$HOME/Dev/Zeugs/tricks.sh"
 
     case "bee"
-        setenv CHECK_DIR_PATH "$HOME/scripts/check"
-        setenv SCRIPTS_DIR_PATH "$HOME/scripts/generic-scripts"
+        set -x CHECK_DIR_PATH "$HOME/scripts/check"
+        set -x SCRIPTS_DIR_PATH "$HOME/scripts/generic-scripts"
 end
 
 switch (uname -n)
@@ -33,18 +33,16 @@ set -gx PATH "$CHECK_DIR_PATH" $PATH
 set -gx PATH "$SCRIPTS_DIR_PATH" $PATH
 set -gx PATH "$HOME/.local/bin" $PATH
 
-setenv LANG 'en_US.UTF-8'
-setenv EDITOR vim
-setenv VISUAL vim
+set -x LANG 'en_US.UTF-8'
+set -x EDITOR vim
+set -x VISUAL vim
 
-setenv N_PREFIX "$HOME/n"  # remove after you run only Ubuntu >=20
+set -x PIP_REQUIRE_VIRTUALENV true
 
-setenv PIP_REQUIRE_VIRTUALENV true
-
-setenv FZF_DEFAULT_COMMAND "find -L . \( -path '*/.git' -o -path '*/.venv' \) -prune -o -print"
-setenv FZF_DEFAULT_OPTS '--bind ctrl-d:page-down,ctrl-u:page-up'
+set -x FZF_DEFAULT_COMMAND "find -L . \( -path '*/.git' -o -path '*/.venv' \) -prune -o -print"
+set -x FZF_DEFAULT_OPTS '--bind ctrl-d:page-down,ctrl-u:page-up'
 if command -v batcat >/dev/null
-    setenv FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'batcat --color=always --style=header,numbers {} 2>/dev/null || tree -Ca {} 2>/dev/null'"
+    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'batcat --color=always --style=header,numbers {} 2>/dev/null || tree -Ca {} 2>/dev/null'"
 else
-    setenv FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'cat {}'"
+    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'cat {}'"
 end

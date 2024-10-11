f64d8c0147c0ad7e7d154f46d3143e2cc46c7a58
Andreas Langenhagen <andreas.langenhagen@wandelbots.com>
2024-10-11 11:12:31 +0200

    Add fish abbr `tq` for `telepresence quit`

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -153,6 +153,7 @@ abbr -a dcs 'docker-compose stop'
 abbr -a dps 'docker ps'
 
 abbr -a tl 'telepresence'
+abbr -a tq 'telepresence quit'
 
 abbr -a d3 "download-m3u8 '"
 abbr -a di3 'download-index-m3u8'

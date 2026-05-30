b13cbbc6ed24e5ce64f978e8d5eed410e187820c
Andreas Langenhagen <andreas@langenhagen.cc>
2026-05-30 21:55:02 +0200

    Move batcat related fish abbr to `ct`

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -49,9 +49,8 @@ abbr -a bp 'bpython'
 abbr -a pv 'python --version'
 abbr -a pe 'python -m venv .venv; source .venv/bin/activate.fish'
 abbr -a ppg 'mkdir foo && cd foo && python -m venv .venv && source .venv/bin/activate.fish && pip install -U pip bpython'
-abbr -a cct 'batcat --number --paging never'  # similar to cat, but via batcat
+abbr -a ct 'batcat --number --paging never'  # similar to cat, but via batcat
 abbr -a vv 'batcat --number --paging never'  # similar to cat, but via batcat
-abbr -a ct 'cat'
 abbr -a f 'find -L . -iname'
 abbr -a l 'lf'
 abbr -a md 'mkdir -p'

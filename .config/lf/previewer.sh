dd51caaa3d9cdeeef4027a58681a2e84276b0fdf
Andreas Langenhagen <andreas@langenhagen.cc>
2026-07-27 18:18:19 +0200

    Make the `lf` previewer more portable

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -5,4 +5,11 @@
 #
 # author: andreasl
 
-command -v batcat && { batcat --color=always --style=numbers,changes,header "$1"; } || cat "$1"
+# Debian ships `bat` under the name `batcat`, Homebrew under its real name
+if command -v batcat >/dev/null; then
+    batcat --color=always --style=numbers,changes,header "$1"
+elif command -v bat >/dev/null; then
+    bat --color=always --style=numbers,changes,header "$1"
+else
+    cat "$1"
+fi

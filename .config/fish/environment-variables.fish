12b968cae631360087d5db8792dda18853e83b2b
Andreas Langenhagen <andreas@langenhagen.cc>
2026-07-27 18:13:48 +0200

    Make `environment-variables.fish` macOS-compatible

--- .config/fish/environment-variables.fish
+++ .config/fish/environment-variables.fish
@@ -4,6 +4,12 @@
 #
 # author: andreasl
 
+# Homebrew must come first: it puts brew's own bin on PATH, which everything
+# below then relies on for finding fish-adjacent tools.
+if test (uname) = 'Darwin'; and test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
 switch (uname -n)
     case "*barn-ultra*" "andreasl-yoga" "*work*"
         set -x BUCKET_LIST_FILE_PATH "$HOME/Barn/Notes/bucket-list.txt"
@@ -38,12 +44,22 @@ set -x LANG 'en_US.UTF-8'
 set -x EDITOR vim
 set -x VISUAL vim
 
+# Debian ships `bat` as `batcat`; Homebrew ships it under its real name.
+# Resolved once here and reused by abbreviations.fish, which is sourced later.
+if command -v batcat >/dev/null
+    set -g bat_cmd batcat
+else if command -v bat >/dev/null
+    set -g bat_cmd bat
+else
+    set -g bat_cmd cat
+end
+
 set -x PIP_REQUIRE_VIRTUALENV true
 
 set -x FZF_DEFAULT_COMMAND "find -L . \( -path '*/.git' -o -path '*/.venv' \) -prune -o -print"
 set -x FZF_DEFAULT_OPTS '--bind ctrl-d:page-down,ctrl-u:page-up'
-if command -v batcat >/dev/null
-    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'batcat --color=always --style=header,numbers {} 2>/dev/null || tree -Ca {} 2>/dev/null'"
+if test "$bat_cmd" != cat
+    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview '$bat_cmd --color=always --style=header,numbers {} 2>/dev/null || tree -Ca {} 2>/dev/null'"
 else
     set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --preview 'cat {}'"
 end
